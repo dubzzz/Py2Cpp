0373ca9dbf75f21e3fd1f3fba27ccbee6c12cf08
Nicolas DUBIEN <github@dubien.org>
2015-06-26 23:09:23 +0200

    Support for: PyObject* to bool, int, long, long long and unsigned versions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 
 #include <Python.h>
@@ -13,12 +14,66 @@ int main(int argc, char* argv[])
     PyObject* out { CppBuilder<PyObject*>()(pyo) };
     std::cout << "PyObject*:\t" << out << std::endl;
   }
+  
+  {
+    // bool
+    PyObject* pyo { Py_True };
+    bool b { CppBuilder<bool>()(pyo) };
+    std::cout << std::boolalpha << "bool:\t" << b << std::endl;
+  }
+  
+  {
+    // int
+    PyObject* pyo { PyLong_FromLong(-50) };
+    int v { CppBuilder<int>()(pyo) };
+    std::cout << "int:\t" << v << std::endl;
+  }
+  
+  {
+    // unsigned int
+    PyObject* pyo { PyLong_FromUnsignedLong(1024) };
+    unsigned int v { CppBuilder<unsigned int>()(pyo) };
+    std::cout << "uint:\t" << v << std::endl;
+  }
+  
+  {
+    // long
+    PyObject* pyo { PyLong_FromLong(-50) };
+    long v { CppBuilder<long>()(pyo) };
+    std::cout << "long:\t" << v << std::endl;
+  }
+  
+  {
+    // unsigned long
+    PyObject* pyo { PyLong_FromUnsignedLong(ULONG_MAX) };
+    unsigned long v { CppBuilder<unsigned long>()(pyo) };
+    std::cout << "ulong:\t" << v << std::endl;
+  }
+  
+  {
+    // long long
+    PyObject* pyo { PyLong_FromLongLong(LLONG_MIN) };
+    long long v { CppBuilder<long long>()(pyo) };
+    std::cout << "long long:\t" << v << std::endl;
+  }
+  
+  {
+    // unsigned long long
+    PyObject* pyo { PyLong_FromUnsignedLongLong(ULLONG_MAX) };
+    unsigned long long v { CppBuilder<unsigned long long>()(pyo) };
+    std::cout << "ulong long:\t" << v << std::endl;
+  }
 
   {
+    // double (from pylong)
+    PyObject* pyo1 { PyLong_FromLong(50) };
+    double v1 { CppBuilder<double>()(pyo1) };
+    std::cout << "double:\t" << v1 << std::endl;
+    
     // double
-    PyObject* pyo { PyFloat_FromDouble(1.1) };
-    double v { CppBuilder<double>()(pyo) };
-    std::cout << "double:\t" << v << std::endl;
+    PyObject* pyo2 { PyFloat_FromDouble(1.1) };
+    double v2 { CppBuilder<double>()(pyo2) };
+    std::cout << "double:\t" << v2 << std::endl;
   }
 
   return 0;
--- src/py2cpp.hpp
+++ src/py2cpp.hpp
@@ -1,4 +1,6 @@
 #include <cassert>
+#include <climits>
+#include <stdexcept>
 
 #include <Python.h>
 
@@ -26,16 +28,128 @@ struct CppBuilder<PyObject*>
 };
 
 template <>
+struct CppBuilder<bool>
+{
+  bool operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyBool_Check(pyo))
+    {
+      return pyo == Py_True;
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<int>
+{
+  int operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      long v { PyLong_AsLong(pyo) };
+      if (v < INT_MIN || v > INT_MAX)
+      {
+        throw std::overflow_error("Out of <int> boundaries");
+      }
+      return static_cast<int>(v);
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<unsigned int>
+{
+  unsigned int operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      unsigned long v { PyLong_AsUnsignedLong(pyo) };
+      if (v > UINT_MAX)
+      {
+        throw std::overflow_error("Out of <unsigned int> boundaries");
+      }
+      return static_cast<unsigned int>(v);
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<long>
+{
+  long operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      return PyLong_AsLong(pyo);
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<unsigned long>
+{
+  unsigned long operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      return PyLong_AsUnsignedLong(pyo);
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<long long>
+{
+  long operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      return PyLong_AsLongLong(pyo);
+    }
+    throw;
+  }
+};
+
+template <>
+struct CppBuilder<unsigned long long>
+{
+  unsigned long operator() (PyObject* pyo)
+  {
+    assert(pyo);
+    if (PyLong_Check(pyo))
+    {
+      return PyLong_AsUnsignedLongLong(pyo);
+    }
+    throw;
+  }
+};
+
+template <>
 struct CppBuilder<double>
 {
   double operator() (PyObject* pyo)
   {
     assert(pyo);
-    if (! PyFloat_Check(pyo))
+    if (PyFloat_Check(pyo))
     {
-      throw;
+      return PyFloat_AS_DOUBLE(pyo);
     }
-    return PyFloat_AS_DOUBLE(pyo);
+    else if (PyLong_Check(pyo))
+    {
+      return PyLong_AsDouble(pyo);
+    }
+    throw;
   }
 };
 
